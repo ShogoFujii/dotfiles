c97417d6451699085db13c8cee7a72852a5acc16
ShogoFujii <shogofujii@MacBook-Air-sf.local>
2016-06-22 19:54:47 +0900

    add shell file

---
+++ dotfilesLink.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+ln -snf ~/git/dotfiles/.vimrc ~/.vimrc
+ln -snf ~/git/dotfiles/.gvimrc ~/.gvimrc
+ln -snf ~/git/dotfiles/.bashrc ~/.bashrc
+ln -snf ~/git/dotfiles/.bash_profile ~/.bash_profile
+ln -snf ~/git/dotfiles/.vim ~/.vim
+ln -snf ~/git/dotfiles/.gitconfig ~/.gitconfig
+ln -snf ~/git/dotfiles/.gitignore_global ~/.gitignore_global
